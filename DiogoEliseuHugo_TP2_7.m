5b8f359713d869c756092a3ffde9a519229d1822
rainydaysavings <eliseuamaro@protonmail.com>
2020-10-05 23:42:49 +0100

    ex7: terminated.

---
+++ DiogoEliseuHugo_TP2_7.m
@@ -0,0 +1,21 @@
+%% Inicialização do ambiente
+clear ; close all; clc
+
+% Definimos uma seed estatica para o gerador de números aleatórios de forma
+% a podermos repetir os experimentos com resultados determinísticos
+rng(42);
+
+%% Exercício 7
+N = 0:80;
+
+s = 2 * N .* (0.9.^N);
+r = rand(1,81) - 0.5;
+x = s + r;
+
+figure
+hold on
+plot(s);
+plot(r);
+plot(x);
+hold off
+DiogoEliseuHugo_TP2_6(s, x)
